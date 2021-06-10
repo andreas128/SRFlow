a48ab43fd27d61c8b3f8f469c418037887c1b2fa
Harrison Wong <hwong557@users.noreply.github.com>
2021-06-10 17:13:30 -0400

    Update setup.sh
    Added `--continue` parameter in case large download gets interrupted.

--- setup.sh
+++ setup.sh
@@ -28,11 +28,11 @@ echo "Download models, data"
 echo "########################################"
 echo ""
 
-wget  http://data.vision.ee.ethz.ch/alugmayr/SRFlow/datasets.zip
+wget --continue http://data.vision.ee.ethz.ch/alugmayr/SRFlow/datasets.zip
 unzip datasets.zip
 rm datasets.zip
 
-wget  http://data.vision.ee.ethz.ch/alugmayr/SRFlow/pretrained_models.zip
+wget --continue http://data.vision.ee.ethz.ch/alugmayr/SRFlow/pretrained_models.zip
 unzip pretrained_models.zip
 rm pretrained_models.zip
 
